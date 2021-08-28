ea23614ae3dc371f2200d998d482b87c3c347bdd
Rai Raksa M <rairaksa7@gmail.com>
2021-08-28 21:43:48 +0700

    update simple landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Spartan'],
+      'serif': ['Merriweather']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
