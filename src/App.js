e62d3c5d2e131ef60b724b9248868aa3ac7c1f87
Rai Raksa M <rairaksa7@gmail.com>
2021-11-27 20:58:04 +0700

    update cv

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ContactSection from './ContactSection';
 
 function App() {
   return (
-    <div className="App bg-dark-60 font-josefin text-silk px-2 lg:px-0 py-2 lg:py-8">
+    <div className="App relative bg-dark-60 font-josefin text-silk px-2 lg:px-0 py-2 lg:py-8">
       <div className="card w-full md:w-1/3 h-auto mx-auto bg-dark-80 pt-24 pb-8 px-4 lg:px-8">
         <section className="profile">
           <ProfileSection />
